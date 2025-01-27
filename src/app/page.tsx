f37ec454caa03a9e8e6199d218ae717fc589dd0c
Dylan Coyne <dylan@dylancoyne.co.uk>
2025-01-27 17:08:03 +0000

    Changed stroke width attribute

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -120,7 +120,7 @@ function CaseStudies({
         <div className="mt-6">
           <a href="/next-app/work" className="inline-flex items-center font-medium text-primary transition hover:text-secondary">View all projects
             <svg className="w-4 h-4 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg>
           </a>
         </div>
@@ -182,7 +182,7 @@ function Services() {
         <div className="mt-6 text-xl">
           <a href="/process" className="inline-flex items-center font-medium text-primary transition hover:text-secondary">How I work
             <svg className="w-4 h-4 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg>
           </a>
         </div>
