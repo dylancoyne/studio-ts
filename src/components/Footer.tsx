eeb242bfecbdcc878b7a727d29b003ec0d2db19d
Dylan Coyne <dylan@dylancoyne.co.uk>
2025-12-10 09:36:28 +0000

    Updates
    Updates to contact details and NPM package and adding React obfuscate

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -92,7 +92,7 @@ function ContactSection() {
           <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
         </svg>
         <p className="mt-4">
-            <a className="text-neutral-700 transition hover:text-secondary" href='mailto:hello@ribboncreative.co.uk'>dylan@ribboncreative.co.uk</a>
+            <a className="text-neutral-700 transition hover:text-secondary" href='mailto:hello@ribboncreative.co.uk'>hello@ribboncreative.co.uk</a>
         </p> 
       <div className="relative mt-8">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
@@ -100,7 +100,7 @@ function ContactSection() {
           <path d="M8.625.75A3.375 3.375 0 0 0 5.25 4.125v15.75a3.375 3.375 0 0 0 3.375 3.375h6.75a3.375 3.375 0 0 0 3.375-3.375V4.125A3.375 3.375 0 0 0 15.375.75h-6.75ZM7.5 4.125C7.5 3.504 8.004 3 8.625 3H9.75v.375c0 .621.504 1.125 1.125 1.125h2.25c.621 0 1.125-.504 1.125-1.125V3h1.125c.621 0 1.125.504 1.125 1.125v15.75c0 .621-.504 1.125-1.125 1.125h-6.75A1.125 1.125 0 0 1 7.5 19.875V4.125Z" />
         </svg>
       <p className="mt-4 text-sm text-neutral-700" >
-     <a href="tel:+447920133043">+44 (0) 7920133043</a>
+     <a href="tel:+447920133043"></a>
       </p>
       </div>
     </div>
